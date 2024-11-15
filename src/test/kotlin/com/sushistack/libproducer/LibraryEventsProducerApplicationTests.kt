ef5e60ec2e0995c807bea90d51a88f25b9a88c7a
sushistack <kjm71712@naver.com>
2024-11-15 15:26:35 +0900

    init library events kafka producer

---
+++ src/test/kotlin/com/sushistack/libproducer/LibraryEventsProducerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sushistack.libproducer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryEventsProducerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
