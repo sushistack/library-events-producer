70834f6ccbc1ddbd526bc08039db39ffb393ad51
shushistack <kjm71712@naver.com>
2024-11-17 21:03:33 +0900

    add producer example

---
+++ src/main/kotlin/com/sushistack/libproducer/producer/LibraryEventsProducer.kt
@@ -0,0 +1,44 @@
+package com.sushistack.libproducer.producer
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.sushistack.libproducer.domain.LibraryEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.stereotype.Component
+import java.util.concurrent.CompletableFuture
+
+@Component
+class LibraryEventsProducer(
+    private val kafkaTemplate: KafkaTemplate<Long, String>,
+    private val objectMapper: ObjectMapper
+) {
+
+    private var log = KotlinLogging.logger {}
+
+    @Value("\${spring.kafka.topic}")
+    lateinit var topic: String
+
+    fun sendLibraryEvent(libraryEvent: LibraryEvent): CompletableFuture<SendResult<Long, String>> {
+        val key = libraryEvent.libraryEventId ?: 0L
+        val value = objectMapper.writeValueAsString(libraryEvent)
+
+        val cf = kafkaTemplate.send(topic, key, value)
+        return cf.whenComplete { sendResult, throwable ->
+            if (throwable != null) {
+                handleFailure(key, value, throwable)
+            } else {
+                handleSuccess(key, value, sendResult)
+            }
+        }
+    }
+
+    private fun handleFailure(key: Long, value: String, throwable: Throwable) {
+        log.error { "Error sending the message and the exception is ${throwable.message}" }
+    }
+
+    private fun handleSuccess(key: Long, value: String, sendResult: SendResult<Long, String>) {
+        log.info { "Message sent successfully for the key := $key and value := $value and partition := ${sendResult.recordMetadata.partition()}" }
+    }
+}
