2e8423ea9c9108f6098b072be52ad07d3a4f48c2
sushistack <kjm71712@naver.com>
2024-11-19 10:47:57 +0900

    add put method

--- src/main/kotlin/com/sushistack/libproducer/controller/LibraryEventControllerAdvice.kt
+++ src/main/kotlin/com/sushistack/libproducer/controller/LibraryEventControllerAdvice.kt
@@ -7,12 +7,11 @@ import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.util.stream.Collectors
 
 
 @ControllerAdvice
-class LibraryEventControllerAdvice: ResponseEntityExceptionHandler() {
+class LibraryEventControllerAdvice {
     private var log = KotlinLogging.logger {}
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
--- src/main/kotlin/com/sushistack/libproducer/controller/LibraryEventsController.kt
+++ src/main/kotlin/com/sushistack/libproducer/controller/LibraryEventsController.kt
@@ -1,12 +1,15 @@
 package com.sushistack.libproducer.controller
 
+import com.fasterxml.jackson.core.JsonProcessingException
 import com.sushistack.libproducer.domain.LibraryEvent
+import com.sushistack.libproducer.domain.LibraryEventType
 import com.sushistack.libproducer.producer.LibraryEventsProducer
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,4 +27,26 @@ class LibraryEventsController(private val libraryEventsProducer: LibraryEventsPr
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent)
     }
 
+    @PutMapping("/v1/libraryevent")
+    @Throws(JsonProcessingException::class)
+    fun putLibraryEvent(@RequestBody libraryEvent: @Valid LibraryEvent): ResponseEntity<*> {
+        val badRequest = validateLibraryEvent(libraryEvent)
+        if (badRequest != null) return badRequest
+
+        libraryEventsProducer.sendLibraryEventApproach2(libraryEvent)
+        log.info("after produce call")
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent)
+    }
+
+    private fun validateLibraryEvent(libraryEvent: LibraryEvent): ResponseEntity<String>? {
+        if (libraryEvent.libraryEventId == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryEventId")
+        }
+
+        if (LibraryEventType.UPDATE != libraryEvent.libraryEventType) {
+            log.info { "Inside the if block" }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event type is supported")
+        }
+        return null
+    }
 }
